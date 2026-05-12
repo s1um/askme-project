e5420eefd2696834f99836eb33ddb3ba60c8db8b
s1um <siuuuum55@gmail.com>
2026-05-13 05:10:23 +0900

    AskMe v1.0 - 오류 수정

--- server/src/index.js
+++ server/src/index.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT ?? 3001;
 
 app.use(cors({
-  origin: process.env.askme-kimminju.up.railway.app || 'http://localhost:5173'
+  origin: process.env.CLIENT_URL || 'http://localhost:5173'
 }));
 app.use(express.json());
 
